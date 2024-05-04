33b9e8cd0b2c63d056c52ed0e7ab99d316580b9b
JiangMing <jm17680291663@163.com>
2024-05-04 22:00:40 +0800

    多进程模型建立

---
+++ include/ngx_proc.h
@@ -0,0 +1,19 @@
+//
+// Created by jm on 4/29/24.
+//
+
+#ifndef JM_NGINX_NGX_PROC_H
+#define JM_NGINX_NGX_PROC_H
+
+// ngx_master_process_loop 主进程loop
+// ngx_swap_process 创建子进程
+
+// ngx_work_process_init 子进程初始化，清空信号集
+// ngx_work_process_loop 子进程loop
+
+void ngx_master_process_loop();
+void ngx_fork_process(int processNums, const char * processName);
+void ngx_work_process_init(const char * processName);
+void ngx_work_process_loop();
+
+#endif //JM_NGINX_NGX_PROC_H
---
+++ proc/ngx_proc.cpp
@@ -0,0 +1,76 @@
+//
+// Created by jm on 4/29/24.
+//
+
+#include "ngx_proc.h"
+#include "log.h"
+#include "ngx_conf.h"
+#include "ngx_func.h"
+
+#include "csignal"
+
+#include <iostream>
+
+const char *WorkProcessName = "jm_nginx:work process";
+
+void ngx_master_process_loop() {
+    sigset_t set;
+    sigemptyset(&set);
+
+    sigaddset(&set, SIGCHLD);   // 僵尸进程
+    sigaddset(&set, SIGALRM);   // 定时器
+    sigaddset(&set, SIGIO);     // 信号驱动IO
+    sigaddset(&set, SIGINT);    // 中断信号
+    sigaddset(&set, SIGHUP);    // 连接断开
+    sigaddset(&set, SIGUSR1);   // 用户自定义1
+    sigaddset(&set, SIGUSR2);   // 用户自定义2
+    sigaddset(&set, SIGWINCH);  // 终端窗口大小改变
+    sigaddset(&set, SIGTERM);   // 终止信号
+    sigaddset(&set, SIGQUIT);   // 终端退出符
+
+    if(sigprocmask(SIG_BLOCK, &set, nullptr) == -1) {
+        LOG_ERROR << "ngx_master_process_loop sigprocmask" << std::endl;
+    }
+
+    int processNums = ngx_conf::getInstance().getInt("WorkProcess", 1);
+    ngx_fork_process(processNums, WorkProcessName);
+    while(1) {
+//        LOG_INFO << "this is parent process" << std::endl;
+    }
+    return ;
+}
+
+void ngx_fork_process(int processNums, const char * processName) {
+    for(int i = 0; i < processNums; ++i) {
+        pid_t pid = fork();
+        switch(pid) {
+            case -1: {
+                LOG_ERROR << "ngx_fork_process fork() error" << std::endl;
+                return ;
+            }
+            case 0:{
+                ngx_work_process_init(processName);
+                ngx_work_process_loop();
+                break;
+            }
+            default :{
+                break;
+            }
+        }
+    }
+}
+
+void ngx_work_process_init(const char * processName) {
+    sigset_t set;
+    sigemptyset(&set);
+    if(sigprocmask(SIG_SETMASK, &set, nullptr) == -1) {
+        LOG_ERROR << "ngx_work_process_init sigprocmask()" << std::endl;
+    }
+    ngx_func::ngx_setProTitle(processName);
+}
+
+void ngx_work_process_loop() {
+    while(1) {
+//        LOG_INFO << "this is work process"<< std::endl;
+    }
+}
